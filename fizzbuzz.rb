d1ceec407668d774c99af714d93d685baf721c59
narai <j1426@matsue-ct.jp>
2018-11-07 11:35:14 +0900

    Add fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,13 @@
 #!/usr/bin/env ruby
 
-
+100.times{ |i|
+  if i%3==0 && i%5==0 
+    puts "FizzBuzz"
+  else if i%3==0
+    puts "Fizz"
+  else if i%5==0
+    puts "Buzz"
+  else
+    puts i
+  end
+}
